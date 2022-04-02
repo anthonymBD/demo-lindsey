9d443787b05a2dc7069de395c1bd6d25353b1f54
amorlett2021 <anthony.morlett@jefferson.edu>
2022-04-01 19:56:03 -0500

    update gsap no npm
    add gsap none npm

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-let tl=gsap.timeline();tl.fromTo("#jumbo h2",{x:-200,opacity:0},{x:0,opacity:1,delay:.6,duration:1}).fromTo(".heroImg",{height:0},{height:"auto",duration:1.8,ease:"power2.inOut"},">-1");
+const gsap=require("gsap");let tl=gsap.timeline();tl.fromTo("#jumbo h2",{x:-200,opacity:0},{x:0,opacity:1,delay:.6,duration:1}).fromTo(".heroImg",{height:0},{height:"auto",duration:1.8,ease:"power2.inOut"},">-1");
 //# sourceMappingURL=script.js.map
